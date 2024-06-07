6b0fe9041306782db6c91b8aed118d746002fe19
Ahmad Amiri <ahmad.amiri.1326@gmail.com>
2024-06-07 16:06:33 +0200

    Benachrichtigung

---
+++ __init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'RRS.apps.RRSConfig'
--- apps.py
+++ apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 class ReviewFeedbackSystemConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ReviewFeedbackSystem'
+
+class RRSConfig(AppConfig):
+    name = 'RRS'
+
+    def ready(self):
+        import RRS.signals
