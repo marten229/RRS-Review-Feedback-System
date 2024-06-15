3cf50a818a5b6a0359af88d219711e7c0e65171a
Marten Schulz <ms@marten-schulz.de>
2024-06-15 12:18:34 +0200

    Unnötige Imports entfernt

--- forms.py
+++ forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Reservation, Bewertung
+from .models import Bewertung
+from ReservationManagement.models import Reservation
 
 class ReservationForm(forms.ModelForm):
     class Meta:
