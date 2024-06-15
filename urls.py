d08c838833870a8834d2b4d3243280afe8b3c95b
Marten Schulz <ms@marten-schulz.de>
2024-06-15 16:17:59 +0200

    Static Image und Name entfernt

--- urls.py
+++ urls.py
@@ -3,8 +3,8 @@ from .views import RestaurantListView, RestaurantDetailView, create_reservation,
 
 urlpatterns = [
     path('restaurants/', RestaurantListView.as_view(), name='restaurant-list'),
-    path('restaurants/<int:pk>/', RestaurantDetailView.as_view(), name='restaurant-detail'),
-    path('restaurants/<int:pk>/reservation/', create_reservation, name='create-reservation'),
+    #path('restaurants/<int:pk>/', RestaurantDetailView.as_view(), name='restaurant-detail'),
+    #path('restaurants/<int:pk>/reservation/', create_reservation, name='create-reservation'),
     path('restaurants/<int:pk>/bewertung/', bewertung_abgeben, name='create-bewertung'),
     path('restaurants/<int:pk>/danke/', danke, name='danke'),
     #path('restaurants/<int:pk>/bewertungen/', restaurant_bewertungen, name='restaurant-bewertungen'),
