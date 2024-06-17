7ea5d2dd072a32f828c0947e4a84b14609e3bda0
Marten Schulz <ms@marten-schulz.de>
2024-06-17 21:12:13 +0200

    Refactor URLs and views for danke page

--- urls.py
+++ urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
-from .views import  danke, delete_bewertung
+from .views import  danke
 
 urlpatterns = [
-    #path('restaurants/', RestaurantListView.as_view(), name='restaurant-list'),
-    #path('restaurants/<int:pk>/', RestaurantDetailView.as_view(), name='restaurant-detail'),
-    #path('restaurants/<int:pk>/reservation/', create_reservation, name='create-reservation'),
-    #path('restaurants/<int:pk>/bewertung/', bewertung_abgeben, name='create-bewertung'),
+
     path('restaurants/<int:pk>/danke/', danke, name='danke'),
-    #path('restaurants/<int:pk>/bewertungen/', restaurant_bewertungen, name='restaurant-bewertungen'),
-    #path('restaurants/delete_bewertung/<int:bewertung_id>/', delete_bewertung, name='delete-bewertung'),
 ]
